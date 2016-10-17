ca5719d8c4bd44bc58ba33b7581c9eb1c18a5ff1
Nathan Long <him@nathanmlong.com>
2016-10-17 12:19:10 -0400

    Fix busted test

--- test/channel_test.exs
+++ test/channel_test.exs
@@ -2,16 +2,16 @@ defmodule Consult.ChannelTest do
   use ChannelCase
 
   setup do
-    {:ok, _, socket} = socket() |> subscribe_and_join(Consult.PanelChannel, "panel_updates")
+    {:ok, _, socket} = socket() |> subscribe_and_join(Consult.PanelChannel, "cs_panel")
     {:ok, socket: socket}
   end
 
   test "sends an updated CS panel" do
     TestApp.Repo.insert!(Fixtures.ongoing_conversation)
     Consult.PanelChannel.send_update
-    assert_broadcast "update", %{body: panel_body}
-    assert Regex.match?(~r/\<h1\>Conversations\<\/h1\>/, panel_body)
-    assert Regex.match?(~r/\<div class="conversation"\>/, panel_body)
+    assert_broadcast "update", %{main_contents: main_contents}
+    assert Regex.match?(~r/\<h1\>Conversations\<\/h1\>/, main_contents)
+    assert Regex.match?(~r/\<div class="conversation"\>/, main_contents)
   end
 
 end
