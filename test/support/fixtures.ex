62afab593fd8c2ae3f13385a0e5bf8f80655c864
Nathan Long <him@nathanmlong.com>
2016-10-21 15:08:07 -0400

    Fix fixture data - must include user role

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -9,7 +9,15 @@ defmodule Fixtures do
     %Conversation {
       tags: Enum.map(tag_names, fn (tag_name) ->
         %Tag{name: tag_name}
-      end)
+      end),
+      messages: [
+        %Message{
+          sender_id: nil,
+          sender_name: "User",
+          sender_role: "user",
+          content: "How much you want for that hamburger menu?",
+        },
+      ]
     }
   end
 
@@ -19,10 +27,12 @@ defmodule Fixtures do
         %Message{
           sender_id: nil,
           sender_name: "User",
+          sender_role: "user",
           content: "Is your maple syrup vegan?"
         },
         %Message{
           sender_id: options.rep_id,
+          sender_role: "representative",
           sender_name: "Rep",
           content: "Yes, and made from free-range trees!"
         },
@@ -36,11 +46,13 @@ defmodule Fixtures do
         %Message{
           sender_id: nil,
           sender_name: "Cleese",
+          sender_role: "user",
           content: "Hello, I would like to buy a fish license, please."
         },
         %Message{
           sender_id: nil,
           sender_name: "Cleese",
+          sender_role: "user",
           content: "He is an halibut."
         },
       ]
@@ -54,16 +66,19 @@ defmodule Fixtures do
         %Message{
           sender_id: nil,
           sender_name: "Alex",
+          sender_role: "user",
           content: "How quickly can you ship a fire extinguisher?"
         },
         %Message{
           sender_id: "rep@example.com",
           sender_name: "Rep",
+          sender_role: "representative",
           content: "Could be there by Tuesday."
         },
         %Message{
           sender_id: nil,
           sender_name: "Alex",
+          sender_role: "user",
           content: "OK. That's [cough] probably fine, thanks!"
         },
       ]
